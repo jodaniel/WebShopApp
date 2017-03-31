b656cab34c74ab948599a144a55a3e894b335971
jodaniel <jodaniel94@gmail.com>
2017-03-30 20:04:13 -0600

    Primer Commit

---
+++ WMarket/WMarket/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Neo4jClient;
+using WMarket.Models;
+
+namespace WMarket.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        /*[HttpPost]
+        public ActionResult ]*/
+
+
+        public void Neo4jConn()
+        {
+            var cliente = new GraphClient(new Uri("http://localhost:7474/db/data"), "neo4j", "neo4j");
+            cliente.Connect();
+        }
+
+        public Usuario GeneraUsuario()
+        {
+            Usuario nUsuario = new Usuario();
+
+
+
+            return nUsuario;
+        }
+    }
+}
---
+++ WMarket/WMarket/Models/Usuario.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WMarket.Models
+{
+
+    public class Usuario
+    {
+        #region Atributos
+
+        int id;
+        String nombreCompleto;
+        String usuario;
+        String contrasena;
+        String telefono;
+        String correo;
+        String direccion;
+        Boolean admin;
+
+
+        #endregion
+
+        #region Propiedades
+
+        public int Id
+        {
+            get  { return this.id; }
+            set { this.id = value; }
+
+        }
+
+        public String NombreCompleto
+        {
+            get { return this.nombreCompleto;}
+            set { this.nombreCompleto = value; }
+        }
+
+        public String User
+        {
+            get { return this.usuario; }
+            set { this.usuario = value; }
+        }
+
+        public String Contrasena
+        {
+            get { return this.contrasena; }
+            set { this.contrasena = value; }
+        }
+
+        public String Telefono
+        {
+            get { return this.telefono; }
+            set { this.telefono = value; }
+        }
+
+        public String Correo
+        {
+            get { return this.correo; }
+            set { this.correo = value; }
+        }
+
+        public String Direccion
+        {
+            get { return this.direccion; }
+            set { this.direccion = value; }
+        }
+
+        public Boolean Admin
+        {
+            get { return this.admin; }
+            set { this.admin = value; }
+        }
+
+        #endregion
+
+        #region Constructor & Destructor
+        public Usuario()
+        { }
+
+        public Usuario(int nId, String nNombreCompleto, String nUser, String nContrasena, String nTelefono, String nCorreo, String nDireccion, Boolean nAdmin)
+        {
+            Id = nId;
+            NombreCompleto = nNombreCompleto;
+            User = nUser;
+            Contrasena = nContrasena;
+            Telefono = nTelefono;
+            Correo = nCorreo;
+            Direccion = nDireccion;
+            Admin = nAdmin;
+        }
+
+        #endregion
+
+        #region Metodos
+        #endregion
+    }
+
+    
+}
