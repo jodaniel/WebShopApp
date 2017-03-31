711018c3666a16d880077a5b936676704cffa0a6
jodaniel <jodaniel94@gmail.com>
2017-03-31 16:50:17 -0600

    MySQL Connect

--- WMarket/WMarket/Controllers/ShopController.cs
+++ WMarket/WMarket/Controllers/ShopController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using WMarket.Models;
 
 namespace WMarket.Controllers
 {
@@ -11,6 +13,27 @@ namespace WMarket.Controllers
         // GET: Shop
         public ActionResult Index()
         {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=127.0.0.1;port=3307;uid=shop;" +
+                "pwd=shop123;database=webmarket;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                List<Producto> productos = new List<Producto>();
+                product
+
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                
+            }
             return View();
         }
     }
