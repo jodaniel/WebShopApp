681bd3484b3b8038c7ec0093d34d905bf7d802bb
Erick <Erick@DESKTOP-PC91016>
2017-04-07 14:24:20 -0600

    Cambio Erick 7Abril 2:43pm

--- WMarket/WMarket/Models/ProCarrito.cs
+++ WMarket/WMarket/Models/ProCarrito.cs
@@ -6,10 +6,13 @@ using System.Web;
 namespace WMarket.Models
 {
     [Serializable]
-    public class Carrito
+    public class ProCarrito
     {
-        public int Id { get; set; }
+        public int Id_prod { get; set; }
+        public int Id_cli { get; set; }
+        public String Nombre { get; set; }
         public decimal Precio { get; set; }
         public int Cantidad { get; set; }
+        public int Modo { get; set; }
     }
 }
