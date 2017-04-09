2e836455be0df1feae1a80b3346cd8a3d3ee349a
Erick <Erick@DESKTOP-PC91016>
2017-04-09 15:07:04 -0600

    Cambios de Imagen

---
+++ WMarket/WMarket/Controllers/ImagenController.cs
@@ -0,0 +1,127 @@
+﻿using WMarket.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace WMarket.Controllers
+{
+    public class ImagenController : Controller
+    {
+        public ActionResult Index()
+        {
+            var theModel = GetThePictures();
+            return View(theModel);
+        }
+
+        public ActionResult AddPicture()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPicture(HttpPostedFileBase theFile)
+        {
+            if (theFile.ContentLength > 0)
+            {
+                string theFileName = Path.GetFileName(theFile.FileName);
+
+                byte[] thePictureAsBytes = new byte[theFile.ContentLength];
+                using (BinaryReader theReader = new BinaryReader(theFile.InputStream))
+                {
+                    thePictureAsBytes = theReader.ReadBytes(theFile.ContentLength);
+                }
+
+                string thePictureDataAsString = Convert.ToBase64String(thePictureAsBytes);
+
+                Imagenes thePicture = new Imagenes()
+                {
+                    FileName = theFileName,
+                    PictureDataAsString = thePictureDataAsString,
+                    codPro = 5
+                };
+                thePicture._id = ObjectId.GenerateNewId();
+
+                bool didItInsert = InsertPictureIntoDatabase(thePicture);
+
+                if (didItInsert)
+                    ViewBag.Message = "La imagen fue actualizada correctamente";
+                else
+                    ViewBag.Message = "Ocurrió un error";
+            }
+            else
+                ViewBag.Message = "Debe subir una imagen";
+
+            return View();
+        }
+
+        private bool InsertPictureIntoDatabase(Imagenes thePicture)
+        {
+            try
+            {
+                var CollecitonDB = GetPictureCollection();
+                var theResult = CollecitonDB.InsertOneAsync(thePicture);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        private List<Imagenes> GetThePictures()
+        {
+            var thePictureColleciton = GetPictureCollection();
+
+            // var filter = Builders<Imagenes>.Filter.Exists(_ => true);
+            var thePictureCursor = thePictureColleciton.Find(new BsonDocument())
+                .Skip(0)
+                .Limit(100)
+                .ToList();
+
+            return thePictureCursor; //;== new List<Imagenes>();
+        }
+
+
+        public async Task<Imagenes> GetAsync(int cod)
+        {
+            var thePictureColleciton = GetPictureCollection();
+            var account = thePictureColleciton.Find(f => f.codPro == cod).FirstAsync();
+            return await account;
+        }
+
+
+        public async Task<FileContentResult> ShowPicture(int cod)
+        {
+            var thePictureColleciton = GetPictureCollection();
+            var thePicture = new Imagenes();
+            thePicture = await GetAsync(cod);
+
+            byte[] thePictureDataAsBytes = Convert.FromBase64String(thePicture.PictureDataAsString);
+
+            return new FileContentResult(thePictureDataAsBytes, "image/jpeg");
+        }
+
+
+
+        private IMongoCollection<Imagenes> GetPictureCollection()
+        {
+
+            var Client = new MongoClient("mongodb://localhost:27017");
+
+            var DB = Client.GetDatabase("Amazon1234");
+
+            var collectionDB = DB.GetCollection<Imagenes>("pictures");
+
+
+            return collectionDB;
+        }
+    }
+}
--- WMarket/WMarket/Models/Imagenes.cs
+++ WMarket/WMarket/Models/Imagenes.cs
@@ -1,47 +1,17 @@
-﻿using System;
+﻿
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
-using MongoDB.Bson;
-using MongoDB.Driver;
-using MongoDB.Driver.GridFS;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace WMarket.Models
 {
     public class Imagenes
     {
-        static void Main(string[] args)
-        {
-            var client = new MongoClient();  //conecta cliente
-            var database = client.GetDatabase("ImageAmazon"); //busca DB
-
-            var fs = new GridFSBucket(database);
-
-            var id = UploadFile(fs); //Funcion para subir
-
-            DownloadFile(fs, id);  //funcion para bajar
-        }
-
-        private static ObjectId UploadFile(GridFSBucket fs)
-        {
-            using (var s = File.OpenRead(@"C:\Users\Erick\Documents\ImagenesURL.txt"))
-            {
-                var t = Task.Run<ObjectId> ( () => {
-                    return fs.UploadFromStreamAsync("ImagenesURL.txt", s);
-                } );
-
-                return t.Result;
-            }
-        }
-
-        private static void DownloadFile(GridFSBucket fs, ObjectId id)
-        {
-            var t = fs.DownloadAsBytesByNameAsync("LaptopHP.png");
-            Task.WaitAll(t);
-            var bytes = t.Result;
-        }
+        public ObjectId _id { get; set; }
+        public string FileName { get; set; }
+        public string PictureDataAsString { get; set; }
+        public int codPro { get; set; }
     }
 }
